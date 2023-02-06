66a4486b4287424c7a5616f856417a2f1ba4170f
Mustofaadni04040 <adniadni04@gmail.com>
2023-02-06 18:10:48 +0700

    behasil membuat nested json

--- res.js
+++ res.js
@@ -5,6 +5,41 @@ exports.ok = function (values, res) {
         "status" : 200,
         "values" : values
     };
+
+    console.log(values)
+
      res.json(data);
      res.end();
 };
+
+//response untuk nested metakuliah
+//values untuk menangkap data jsonnya
+
+exports.oknested = function (values, res) {
+    //lakukan akumulasi
+    const hasil = values.reduce((akumulasikan, item) => {
+        //tentukan key group
+        if(akumulasikan[item.nama]) {
+            //buat variable group nama mahasiswa 
+            const group = akumulasikan[item.nama];
+            //cek jika isi array adalah matakuliah
+            if(Array.isArray(group.matakuliah)) {
+                //tambahkan value kedalam group matakuliah
+                group.matakuliah.push(item.matakuliah);
+            }else {
+                group.matakuliah = [group.matakuliah, item.matakuliah];
+            }
+        }else {
+            akumulasikan[item.nama] = item;
+        }
+        return akumulasikan;
+    }, {});
+
+    var data = {
+        "status" : 200,
+        "values" : hasil
+    };
+
+    res.json(data);
+    res.end();
+}
--- routes.js
+++ routes.js
@@ -23,4 +23,7 @@ module.exports = function (app) {
 
     app.route("/hapus")
         .delete(jsonku.hapusMahasiswa);
+
+    app.route("/tampilmatakuliah")
+        .get(jsonku.tampilgroupmatakuliah);
 }
